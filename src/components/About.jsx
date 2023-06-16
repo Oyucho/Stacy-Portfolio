ddc8aabf44971e31e74499f6b1a087e300fffcb2
Oyucho <bellahoyucho30@gmail.com>
2023-06-16 11:08:18 +0300

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+import { SectionWrapper } from '../hoc';
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <motion.div
+      variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
+      className="xs:w-[250px] w-full card-gradient p-[1px] rounded-[20px] shadow-card">
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-jetLight rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+        <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+        <h3 className="text-taupe text-[18px] font-bold text-center">
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  );
+};
+
+const About = () => {
+  return (
+    <div className="-mt-[6rem]">
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn('', '', 0.1, 1)}
+        className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]">
+        As a recent BSc. Mathematics and Computer Science graduate, I am driven by the fascinating intersection of technology with other fields, particularly policies and human rights. My passion lies in leveraging technology to create sustainable, community-centered solutions.
+        With experience in web application development and working with technologies like Internet of Things (IoT), I have developed a strong foundation in applying technical skills to address real-world challenges. 
+        <br></br>
+        <br></br>Additionally, I find joy in mentoring young high school girls in app development, aiming to ignite their curiosity in technology and contribute towards bridging the digital divide.
+        I am committed to making a positive impact by utilizing my technical expertise and collaborating with diverse stakeholders. I aspire to create innovative solutions that not only meet the needs of communities but also align with ethical and inclusive practices.
+        Let's connect and explore how we can collaborate to bring about meaningful change through the power of technology.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SectionWrapper(About, 'about');
